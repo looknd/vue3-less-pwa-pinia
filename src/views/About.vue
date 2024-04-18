892d7c405a7e1c235a3d623e09ec01798e13c061
仔仔 <1375560445@qq.com>
2024-04-18 15:03:41 +0800

    VUE3基本配置 pinia

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>这个数字是：{{count}}</h1>
+    <p>
+      <button @click="hander">增加</button>
+    </p>
   </div>
 </template>
+<!-- script setup模式 -->
+<script setup>
+import {computed} from 'vue'
+import {useCounterStore} from '../store/index'
+
+    const useState = useCounterStore()
+    const hander = ()=>{
+      useState.increment()
+    }
+    const count=computed(()=>useState.count)
+
+</script>
+
+<!-- setup hooks 模式 -->
+<!-- <script>
+import {computed} from 'vue'
+import {useCounterStore} from '../store/index'
+export default {
+  setup() {
+    const useState = useCounterStore()
+    const hander = ()=>{
+      useState.increment()
+    }
+    return{
+      count:computed(()=>useState.count),
+      hander
+    }
+  },
+}
+</script> -->
